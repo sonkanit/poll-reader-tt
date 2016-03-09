c4e1cb7b9780690ecd37b2abae3fa30361c19464
Kanit Mekritthikrai <kanit.mekritthikrai@thomsonreuters.com>
2016-03-09 09:35:24 +0700

    more

---
+++ run.bat
@@ -0,0 +1,5 @@
+REM FOR %%i IN (.\Data\kanthong*.jpg) DO .\Release\PollReader.exe %%i .\Data\kanthong.txt .\kanthong.csv
+REM FOR %%i IN (.\Data\klongnongyai-kanchamas*.jpg) DO .\Release\PollReader.exe "%%i" .\Data\klongnongyai.txt .\klongnongyai-kachamas.csv
+REM FOR %%i IN (.\Data\klongnongyai-kasidis*.jpg) DO .\Release\PollReader.exe %%i .\Data\klongnongyai.txt .\klongnongyai-kasidis.csv
+REM FOR %%i IN (.\Data\klongnongyai-theeraphap*.jpg) DO .\Release\PollReader.exe %%i .\Data\klongnongyai.txt .\klongnongyai-theeraphap.csv
+FOR %%i IN (.\Data\klongnongyai-theeraphap*.jpg) DO .\Release\PollReader.exe %%i .\Data\klongnongyai.txt .\klongnongyai-theeraphap.csv
